71c493c6c24e812c4be1d35b07fcff1da05d7bb4
Tofajjol Hosen Raju <rajukhan894200@gmail.com>
2025-10-14 10:28:13 +0600

    Api call

---
+++ src/ApiCall.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+const ApiCall = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+
+      const data = await response.json();
+      setUsers(data);
+    };
+
+    fetchUser();
+  }, []);
+
+  console.log("All", users);
+
+  return (
+    <div className=" h-screen">
+      Call from API
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+        {
+        users.map((user) => (
+          <div className="p-5 border border-green-600">
+            <h1>Name: {user.name}</h1>
+            <p>Username: @{user.username}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+          </div>
+        ))
+        }
+      </div>
+    </div>
+  );
+};
+
+export default ApiCall;
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ApiCall from "./ApiCall";
 
 const App = () => {
   const [count, setCount] = useState(0); // store kore rakha
@@ -13,32 +14,35 @@ const App = () => {
     return () => {
       console.log("Cleand up!");
     };
-  }, [count]);// dependency list
+  }, [count]); // dependency list
 
   return (
-    <div className="flex  justify-center items-center h-screen">
-      <div>
-        <h1 className="text-4xl py-5 text-center">Count: {count}</h1>
-        <button
-          onClick={() => setCount(count + 1)}
-          className=" border border-green-600 px-5 mr-5"
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => setCount(0)}
-          className=" border border-blue-600 px-5 mx-5"
-        >
-          Reset
-        </button>
-        <button
-          onClick={() => setCount(count - 1)}
-          disabled={count == 0 ? true : false}
-          className=" border border-red-600 px-5 ml-5"
-        >
-          Decrement
-        </button>
+    <div>
+      <div className="flex  justify-center items-center h-screen">
+        <div>
+          <h1 className="text-4xl py-5 text-center">Count: {count}</h1>
+          <button
+            onClick={() => setCount(count + 1)}
+            className=" border border-green-600 px-5 mr-5"
+          >
+            Increment
+          </button>
+          <button
+            onClick={() => setCount(0)}
+            className=" border border-blue-600 px-5 mx-5"
+          >
+            Reset
+          </button>
+          <button
+            onClick={() => setCount(count - 1)}
+            disabled={count == 0 ? true : false}
+            className=" border border-red-600 px-5 ml-5"
+          >
+            Decrement
+          </button>
+        </div>
       </div>
+      <ApiCall />
     </div>
   );
 };
