f93ff41111c59892c872626abd6955a110249e37
Tofajjol Hosen Raju <rajukhan894200@gmail.com>
2025-10-14 10:00:32 +0600

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
-import "./App.css";
-import Card from "./components/Card/Card";
+import { useEffect, useState } from "react";
 
-function App() {
-  const data = {
-    img: "https://imag.jpg",
-    title: "Product Name 1",
-    price: "5000",
-    ratings: "5.5",
-  };
+const App = () => {
+  const [count, setCount] = useState(0); // store kore rakha
+
+  // useEffect(()=> {}, [])
+
+  useEffect(() => {
+    // main code to run
+    console.log(count);
+
+    // return optional function
+    return () => {
+      console.log("Cleand up!");
+    };
+  }, [count]);// dependency list
 
   return (
-    <>
-      <Card cardData={data} />
-      <h1>Vite + React</h1>
-    </>
+    <div className="flex  justify-center items-center h-screen">
+      <div>
+        <h1 className="text-4xl py-5 text-center">Count: {count}</h1>
+        <button
+          onClick={() => setCount(count + 1)}
+          className=" border border-green-600 px-5 mr-5"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => setCount(0)}
+          className=" border border-blue-600 px-5 mx-5"
+        >
+          Reset
+        </button>
+        <button
+          onClick={() => setCount(count - 1)}
+          disabled={count == 0 ? true : false}
+          className=" border border-red-600 px-5 ml-5"
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
